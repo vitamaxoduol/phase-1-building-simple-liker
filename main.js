1e15ba50c6113e1810de82ae09685d3a664fdb3b
Vitamax Oduol <85936735+vitamaxoduol@users.noreply.github.com>
2023-07-03 14:46:19 +0300

    Completed lab

--- main.js
+++ main.js
@@ -2,8 +2,48 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
 
+// Get the error modal and message elements
+const errorModal = document.getElementById('modal');
+const errorMessage = document.getElementById('modal-message');
+
+// Hide the error modal initially
+errorModal.classList.add('hidden');
+
+// Add event listener to all like buttons
+const likeButtons = document.getElementsByClassName('like-glyph');
+for (let button of likeButtons) {
+  button.addEventListener('click', handleLike);
+}
+
+// Function to handle the like button click event
+function handleLike(event) {
+  const heart = event.target;
+
+  // Check if the heart is empty or full
+  if (heart.textContent === EMPTY_HEART) {
+    mimicServerCall()
+      .then(() => {
+        // Change the heart to full and add activated-heart class
+        heart.textContent = FULL_HEART;
+        heart.classList.add('activated-heart');
+      })
+      .catch((error) => {
+        // Display the error message in the modal
+        errorMessage.textContent = error;
+        // Remove the hidden class to display the error modal
+        errorModal.classList.remove('hidden');
+        // Hide the error modal after 3 seconds
+        setTimeout(() => {
+          errorModal.classList.add('hidden');
+        }, 3000);
+      });
+  } else {
+    // Change the heart back to empty and remove activated-heart class
+    heart.textContent = EMPTY_HEART;
+    heart.classList.remove('activated-heart');
+  }
+}
 
 
 
